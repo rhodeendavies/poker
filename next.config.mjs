31ad981edf25d6e66d3e3899f62785501f09d6fa
Rhodeen Davies <rhodeendavies@gmail.com>
2024-08-28 19:02:45 +0200

    patch poker-evaluator

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	experimental: {
-		serverComponentsExternalPackages: ["poker-evaluator"],
+		serverComponentsExternalPackages: ["poker-evaluator"]
 	}
 };
 
